6e9aac68e07349c63add68f09422d65206ac8d85
Takarada0202 <joun0925lim@gmail.com>
2022-08-17 00:16:13 +0900

    first

---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="header">
+    <router-link to="/">Home</router-link>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#header {
+    width: auto;
+    border: 1px solid black;
+    padding: 20px;
+    font-size: 20px;
+  }
+  #header a { 
+    font-style: none;
+    color: black;
+    margin: 0 10px;
+    text-decoration: none;
+  }
+</style>
---
+++ src/page/BoardListPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    {{userBoardList}}
+    {{userId}}
+  </div>
+</template>
+
+<script>
+export default {
+     data() {
+        return {
+            userId : this.$route.params.id,
+            userBoardList : null,
+        }
+    },
+    methods: {
+        fetchData: function() {
+            let self = this;
+
+            this.$axios.get('https://jsonplaceholder.typicode.com/todos/?userId='+this.userId)
+            .then(function(response) {
+                // response
+                if(response.status !== 200){
+                    alert('통신에러!');
+                    self.userBoardList = [];
+                }else{
+                    self.userBoardList = response.data;
+                }
+            })
+            // .catch(function(error) {
+            // console.log(error);
+            // });
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
